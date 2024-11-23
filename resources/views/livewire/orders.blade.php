7f0e040ba015e16cc43811c39490137974e2e125
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2024-11-24 00:46:54 +0200

    update orders

--- resources/views/livewire/orders.blade.php
+++ resources/views/livewire/orders.blade.php
@@ -15,7 +15,7 @@
                                 <table>
                                     <thead>
                                         <tr>
-                                            <th class="pro-remove">Remove</th>
+                                            <th class="pro-remove">ID</th>
                                             <th class="pro-thumbnail">Image</th>
                                             <th class="pro-title">Product</th>
                                             <th class="pro-price">Unit Price</th>
@@ -24,30 +24,48 @@
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        @foreach ($orders as $order)
                                         <tr>
                                             <td class="pro-remove">
                                                 <a href="#">×</a>
                                             </td>
                                             <td class="pro-thumbnail">
-                                                <a href="#">
-                                                    <img src="img/product/2.jpg" alt="" />
-                                                </a>
+
+                                                <img src="{{" /storage/".$order->product->feature_image}}" alt="" />
+
                                             </td>
                                             <td class="pro-title">
-                                                <a href="#">DSR Eiffel chair</a>
+                                                {{$order->product->name}}
                                             </td>
                                             <td class="pro-price">
-                                                <span class="amount">$137.00</span>
+                                                <span class="amount">${{$order->total}}</span>
                                             </td>
                                             <td class="pro-stock-stauts">
-                                                <span class="out-stock">out stock</span>
+                                                <span class="out-stock">{{$order->status}}</span>
                                             </td>
-                                            <td class="pro-add-to-cart">
-                                                <a href="#" class="add-to-cart">
-                                                    add to cart
+                                            <td>
+                                                <a href="#">
+                                                    @php
+                                                    $data = json_decode($order->fields);
+                                                    $data = json_decode($data);
+                                                    @endphp
+                                                    @foreach ($data as $fild)
+
+                                                    <div class="short-desc section">
+                                                        <h5 class="pd-sub-title">
+                                                            Field Name : {{$fild->name}}
+                                                        </h5>
+                                                        <p>
+                                                            Field Value : {{$fild->value}}
+                                                        </p>
+                                                    </div>
+
+                                                    @endforeach
                                                 </a>
                                             </td>
                                         </tr>
+
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
