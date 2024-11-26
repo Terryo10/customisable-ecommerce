5eea57be9fe46e9fad0727f70152a4a627e4e181
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2024-11-26 16:29:15 +0200

    update

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Orders;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
@@ -18,6 +19,15 @@ class HomeController extends Controller
             ->row(view('title'))
             ->row(function (Row $row) {
 
+                $row->column(4, function (Column $column) {
+
+                    $orders = array_fill(0, 12, 0);
+                    $data = Orders::selectRaw('COUNT(*) as count, YEAR(created_at) year, MONTH(created_at) month')->groupBy('year', 'month')->get();
+                    foreach ($data as $key => $value) {
+                        $orders[$value->month - 1] = $value->count;
+                    }
+                    $column->append(view('admin.chart')->with('orders', $orders));
+                });
             });
     }
 }
