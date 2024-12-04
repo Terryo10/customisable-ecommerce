bd48c8f3509e6a52f2b8eee343f74c68e4211175
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2024-12-04 23:28:14 +0200

    update

--- resources/views/livewire/slider.blade.php
+++ resources/views/livewire/slider.blade.php
@@ -7,19 +7,19 @@
                     <!-- Home Text Content -->
                     @foreach ($sliders as $slider)
                     <div class="home-slick-content">
-                        <h1>jumpsuit</h1>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, ay dil he moskil.</p>
-                        <a href="#" class="btn">SHOP NOW</a>
+                        <h1>{{ $slider->title ?? "" }}</h1>
+                        <p>{{ $slider->description ?? "" }}</p>
+                        <a href="#shop" class="btn">View Shop</a>
                     </div>
                     @endforeach
                 </div>
                 <!-- Home Slick Image Slider -->
                 <div class="home-slick-image-slider col-sm-6 col-xs-12">
                     @foreach ($sliders as $slider)
-                    <div class="image"><img src="/storage/{{$slider->image }}" alt="" style="height: 70%"></div>
+                    <div class="image"><img src="/storage/{{ $slider->image }}" alt="" style="height: 70%"></div>
                     @endforeach
-                
-                   
+
+
                 </div>
             </div>
         </div>
