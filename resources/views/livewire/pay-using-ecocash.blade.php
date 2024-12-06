b6881fba175c651ac2359e405de8f56c91b27328
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2024-12-06 14:36:28 +0200

    update app

--- resources/views/livewire/pay-using-ecocash.blade.php
+++ resources/views/livewire/pay-using-ecocash.blade.php
@@ -35,7 +35,7 @@
                                 placeholder="Enter your phone number">
                             @error('phone') <span class="text-danger">{{ $message }}</span> @enderror
                         </div>
-                        <button type="submit" class="btn btn-primary mb-3" wire:loading.attr="disabled">
+                        <button type="submit" class="btn btn-primary mb-3 form-control" wire:loading.attr="disabled">
                             <span wire:loading.remove>Pay Now Using Ecocash</span>
                             <span wire:loading>Please wait...</span>
                         </button>
@@ -43,7 +43,7 @@
                     </form>
 
                     @if ($paymentSent === "true")
-                    <button class="btn btn-warning" wire:click="checkPayment" wire:loading.attr="disabled">
+                    <button class="btn btn-warning form-control" wire:click="checkPayment" wire:loading.attr="disabled">
                         <span wire:loading.remove>Check your initiated payment</span>
                         <span wire:loading>Checking...</span>
                     </button>
