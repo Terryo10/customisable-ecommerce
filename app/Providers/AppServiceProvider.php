d0e767771248cbcc849c4d32c838ea02acd6c0a9
Terryo10 <tapiwatererai@outlook.com>
2025-01-07 21:35:15 +0200

    app models unguard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        static::unguard();
+        Model::unguard();
 
         app()->bind('path.public', function () {
             return realpath(base_path('public'));
